00fe01992cdcdbce715050a89b45b16289cf3552
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2020-12-13 13:07:05 -0600

    commit tree 270fe5c15133b7b1a06572073e5e74f053838cfe

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,10 @@
+class CashRegister
+    attr_accessor :total, :discount, :title
 
+    #optional discount upon initialization
+    def initialize(discount = nil)
+        @total = 0
+        @discount = discount
+    end
+
+end
